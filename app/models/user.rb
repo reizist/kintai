188c245d8d5f7a7dfeb27c11c060d71197050e7f
reizist <reizist@gmail.com>
2017-01-07 21:46:02 +0900

    :construction: Implement google auth communication method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,21 @@
 class User < ApplicationRecord
-  devise :trackable, :omniauthable
+  devise :trackable, :omniauthable, omniauth_providers: %i(google)
+
+  protected
+
+  def self.find_for_google(auth)
+    user = User.find_or_initialize_by(email: auth.info.email)
+
+    if user.new_record?
+      user = User.create(
+        name:     auth.info.name,
+        provider: auth.provider,
+        uid:      auth.uid,
+        token:    auth.credentials.token,
+        password: Devise.friendly_token[0, 20],
+        meta:     auth.to_yaml
+      )
+    end
+    user
+  end
 end
